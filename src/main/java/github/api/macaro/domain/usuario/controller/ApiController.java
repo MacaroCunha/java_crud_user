447bc5d9ccad62cadbc58246dd3bcd31f77654f7
MacaroCunha <xmariacaroline@outlook.com>
2024-02-03 17:53:11 -0300

    feat: c do crud implementado, pendente camada de service e DTO

--- src/main/java/github/api/macaro/domain/usuario/controller/ApiController.java
+++ src/main/java/github/api/macaro/domain/usuario/controller/ApiController.java
@@ -3,13 +3,9 @@ package github.api.macaro.domain.usuario.controller;
 import github.api.macaro.domain.usuario.model.Usuario;
 import github.api.macaro.domain.usuario.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class ApiController {
@@ -18,7 +14,11 @@ public class ApiController {
 
     @GetMapping("/buscarUser")
     public @ResponseBody List<Usuario> buscarUsuario() {
-        List<Usuario> us = this.repository.findAll();
-        return us;
+        return this.repository.findAll();
+    }
+    
+    @PostMapping("createUser")
+    public Usuario criarUser(@RequestBody Usuario source){
+        return this.repository.save(source);
     }
 }
